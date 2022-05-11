bbca0d4a65d8aa32380307f047a74535555ea005
Emmanuel Guerrero <emanuel.gue@gmail.com>
2022-05-11 08:29:27 -0500

    setting up and first steps

---
+++ etch.js
@@ -0,0 +1,60 @@
+//get body element to add to it
+const body = document.body
+
+//document fragment to add all the divs to add that to container and that to body
+const fragment = document.createDocumentFragment();
+
+
+
+//array with 16x16 divs
+let divs = [
+    createDiv("div1"),
+    createDiv("div2"),
+    createDiv("div3"),
+    createDiv("div4"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    createDiv("div1"),
+    
+]
+
+//function that creates div element
+function createDiv() {
+    let div = document.createElement("div");
+    
+}
+
+//loop to add the 16x16 divs to the fragment 
+
+
+//div with container class to hold grid
+const conatiner = document.createElement("div");
+conatiner.classList.add("container");
+
+//add all the divs to the container 
+div 
+
+
+
+
+
+
